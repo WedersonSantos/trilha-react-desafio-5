29106c2886435ef0ffa9cac12592d032596198d5
Wederson Santos <wedersonsantos2004@gmail.com>
2024-10-22 18:08:29 -0300

    Update CustomLink.js

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -1,9 +1,10 @@
+
 import Link from 'next/link';
 
 export default function CustomLink({ as, href, ...otherProps }) {
   return (
     <>
-      <Link as={as} href={href}>
+      <Link  as={as} href={href} legacyBehavior>
         <a {...otherProps} />
       </Link>
     </>
